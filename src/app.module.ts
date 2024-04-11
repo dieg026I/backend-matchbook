d35915a8965db9b7b88cdb870a9087af40cf3a9c
dieg026I <dieg.isla@duocuc.cl>
2024-04-10 23:26:49 -0400

    avance_conexion_BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'dieg026',
+    password: '95809580Dd',
+    database: '',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true, // Solo para entornos de desarrollo
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  rut: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  phone: number;
+
+ 
+
+
+}
+
+
